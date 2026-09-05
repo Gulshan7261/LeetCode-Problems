f7bdd09f6cce84cc3f809e1de20b6db43d7a083a
Gulshan7261 <gk5120909@gmail.com>
2026-09-05 18:50:23 +0530

    Time: 0 ms (100%), Space: 8.5 MB (16.36%) - LeetHub

---
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool checkRecord(string s) {
+        int absent=0;int late=0;
+        for(char ch:s){
+            if(ch == 'A'){
+                absent++;
+                if(absent >=2)
+                return false;
+            }
+            if(ch == 'L'){
+                late++;
+                if(late >= 3) return false;
+
+            }
+            else
+            late = 0;
+        }
+        return true;
+        
+    }
+};
