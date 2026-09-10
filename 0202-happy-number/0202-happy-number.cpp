0d144124a0f7a7428ae0306ca757798e3dcd8970
Gulshan7261 <gk5120909@gmail.com>
2026-09-10 19:46:42 +0530

    Time: 1 ms (26.53%), Space: 8.4 MB (36.97%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        std::unordered_set<int> usedIntegers;
+        while (n != 1 && !usedIntegers.count(n)) {
+            usedIntegers.insert(n);
+            int sum = 0;
+            while(n > 0){
+                int digit = n % 10;
+                sum += digit * digit;
+                n = n/10;
+            }
+            n = sum;
+
+            }
+             return n == 1;
+
+        }
+};
